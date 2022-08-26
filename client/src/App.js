b27535232d45024cfae242677c38919faf96ff9c
andrey <elefant86@bk.ru>
2022-08-26 14:33:35 +0300

    add items to cart ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Items from './components/Items';
+import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Items from './components/Items'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
+	constructor(props) {
+		super(props)
+		this.state = {
+			orders: [],
 			items: [
 				{
 					id: 1,
 					title: 'Стул серый',
 					img: 'denys-striyeshyn-wJ7yGwz2-00-unsplash.jpg',
-					desc: 'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона',
+					desc:
+						'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона',
 					category: 'Стулья',
 					price: '49.90',
 				},
@@ -20,7 +22,8 @@ class App extends React.Component {
 					id: 2,
 					title: 'Стул серый',
 					img: 'goashape-RvyJGaoaWrs-unsplash.jpg',
-					desc: 'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона',
+					desc:
+						'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона',
 					category: 'Стулья',
 					price: '49.90',
 				},
@@ -28,7 +31,8 @@ class App extends React.Component {
 					id: 3,
 					title: 'Стул серый',
 					img: 'inside-weather-BayIzHKkjM4-unsplash.jpg',
-					desc: 'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона',
+					desc:
+						'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона',
 					category: 'Стулья',
 					price: '49.90',
 				},
@@ -36,7 +40,8 @@ class App extends React.Component {
 					id: 4,
 					title: 'Стул серый',
 					img: 'jean-philippe-delberghe-xrjusFfOksI-unsplash.jpg',
-					desc: 'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона',
+					desc:
+						'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона',
 					category: 'Стулья',
 					price: '49.90',
 				},
@@ -44,7 +49,8 @@ class App extends React.Component {
 					id: 5,
 					title: 'Стул серый',
 					img: 'khloe-arledge-8Rz_RIyp5FM-unsplash.jpg',
-					desc: 'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона',
+					desc:
+						'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона',
 					category: 'Стулья',
 					price: '49.90',
 				},
@@ -52,23 +58,33 @@ class App extends React.Component {
 					id: 6,
 					title: 'Стул серый',
 					img: 'maiar-shalaby-faJf1aDiezQ-unsplash.jpg',
-					desc: 'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона',
+					desc:
+						'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона',
 					category: 'Стулья',
 					price: '49.90',
 				},
 			],
 		}
-  }
+		this.addToOrder = this.addToOrder.bind(this) // за счет это строчки в методе addToOrder мы сможем взаимодействовать с состояниями (orders, items выше)
+	}
 
-  render () {
-    return (
-      <div className='wrapper'>
-        <Header/>
-        <Items items={this.state.items}/>
-        <Footer/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='wrapper'>
+				<Header orders={this.state.orders} />
+				<Items items={this.state.items} onAdd={this.addToOrder} />
+				<Footer />
+			</div>
+		)
+	}
+
+	addToOrder(item) {
+		let isInArray = false
+		this.state.orders.forEach((el) => {
+			if (el.id === item.id) isInArray = true
+		})
+		if (!isInArray) this.setState({ orders: [...this.state.orders, item] })
+	}
 }
 
-export default App;
+export default App
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react'
 import { FaShoppingCart } from 'react-icons/fa'
+import Order from './Order'
 
-export default function Header() {
+const showOrders = (props) => {
+	return (
+		<div>
+			{props.orders.map((el) => (
+				<Order key={el.id} item={el} />
+			))}
+		</div>
+	)
+}
+
+const showNothing = () => {
+	return (
+		<div className='empty'>
+			<h2>Товаров нет</h2>
+		</div>
+	)
+}
+
+export default function Header(props) {
 	let [cartOpen, setCartOpen] = useState(false)
 	return (
 		<header>
@@ -17,6 +36,12 @@ export default function Header() {
 					className={`shop-cart-button ${cartOpen && 'active'}`}
 				/>
 				{/* если cartOpen - true, то к классу добавиться класс active */}
+
+				{cartOpen && (
+					<div className='shop-cart'>
+						{props.orders.length > 0 ? showOrders(props) : showNothing()}
+					</div>
+				)}
 			</div>
 			<div className='presentation'></div>
 		</header>
